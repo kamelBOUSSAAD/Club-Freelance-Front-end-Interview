437ad322fab7ad3a46e0410a7a7b33b3373c3b25
kamelBOUSSAAD <44210916+kamelBOUSSAAD@users.noreply.github.com>
2019-01-01 00:32:14 +0100

    DELETE UPDATE

--- jquery.js
+++ jquery.js
@@ -1,59 +1,51 @@
-function getBloges(siteID){
-   
-    $.ajax({
-        url: 'http://clubfreetst.herokuapp.com/blogs/'+siteID,
-        data: {
-           format: 'html'
-        },
-        error: function() {
-           $('#blog').html('<p>An error has occurred</p>');
-        },
-        dataType: 'json',
+function getBloges(siteID) {
+  $("#blog").remove();
 
-        success: function(itemData) {
-         $('#error').remove()
-           
-            $('#note').html('<h2> Blogger : '+itemData.blogger+'</h2> ')
-            $('#note').append('<h2> Title : '+itemData.title+'</h2>')
-           
-            $('#note').append('<h2 class="note-title"> Notes </h2>')
-            for(var i=0;i<=itemData.notes.length-1;i++)
-            {
-               var id = itemData.notes[i].id;
-               var title = itemData.notes[i].title;
-           
-              
-               $('#note').append('<li> Title : '+title+' <button  onClick="deleteNote(document.getElementById('+id+').value)">delete </button> </li>')
-              
-               
-              }   
-            },
-        type: 'GET'
-     });
-    } 
-   
+  $.ajax({
+    url: "http://clubfreetst.herokuapp.com/blogs/" + siteID,
+    data: {
+      format: "html"
+    },
+    error: function() {
+      $("#blog").html("<p>An error has occurred</p>");
+    },
+    dataType: "json",
 
+    success: function(itemData) {
+      $("#error").hide();
 
+      $("#note").html("<h2> Blogger : " + itemData.blogger + "</h2> ");
+      $("#note").append("<h2> Title : " + itemData.title + "</h2>");
 
- 
+      $("#note")
+        .append('<h2 class="note-title"> Notes </h2>')
+        .text();
+      for (var i = 0; i <= itemData.notes.length - 1; i++) {
+        var id = itemData.notes[i].id;
+        var title = itemData.notes[i].title;
 
- 
-function deleteNote(id){
-   alert('fghj')
-   $.ajax({
-      url: 'http://clubfreetst.herokuapp.com/notes/'+id,
-      error: function() {
-      $('#delete').append('<h3>erruer<h3> ')
-      },
-      
-      type: 'DELETE',
-      success: function(res) {
-         $('#delete').append('<h3>OK <h3> ')
-      },
-      error: function() {
-      $('#delete').append('<h3>erruer<h3> ')
+        $("#note").append(`<li> Title : ` + title + ` </li>`);
+        $("#note").append(
+          `<button class="delete-button" onClick="delete_item('` +
+            id +
+            `')">Delete  X </button> `
+        );
       }
-   })                 
+    },
+    type: "GET"
+  });
+}
 
-
+function delete_item(id) {
+  jQuery.ajax({
+    url: "http://clubfreetst.herokuapp.com/notes/" + id,
+    type: "DELETE",
+    data: { _method: "delete" },
+    success: function(data) {
+      getBloges(document.getElementById("siteID").value);
+    },
+    error: function(err) {
+      $("#delete").append("<h3>" + JSON.stringify(err) + "<h3> ");
+    }
+  });
 }
--- js.js
+++ js.js
@@ -1,32 +1,20 @@
-
-
-   function isValid(number){
-  
-    var nbStr = number.toString();
-    if ((nbStr.length >=5)&&(nbStr.length <=7)) 
-       {
-        if(!(number%5 == 0)) 
-          {
-            return true
-          }
-             else 
-            {
-              $('#error').html('<p>Error input : siteID divisible by 5 will not be found</p>');
-             
-             return false
-            }
-        
-        
-       } else
-         {
-            $('#error').html('<p>Error input : the length of siteID must be between 5 and 7</p>');
-            return false
-         }
-
-
-        }
-
-
-
-
-
+function isValid(number) {
+  var nbStr = number.toString();
+  if (nbStr.length >= 5 && nbStr.length <= 7) {
+    if (!(number % 5 == 0)) {
+      return true;
+    } else {
+      $("#error").html(
+        "<p>Error input : siteID divisible by 5 will not be found</p>"
+      );
+      $("#error").show();
+      return false;
+    }
+  } else {
+    $("#error").html(
+      "<p>Error input : the length of siteID must be between 5 and 7</p>"
+    );
+    $("#error").show();
+    return false;
+  }
+}
